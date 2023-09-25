7322d48718dd0d1e9139ec26afa3699befbf2fc7
Alexander Shapoval <kiljcore@gmail.com>
2023-09-25 12:58:35 +0300

    Added basic assets.

---
+++ Source/May/May.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "May.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, May, "May" );
+
+DEFINE_LOG_CATEGORY(LogMay)
+ 
