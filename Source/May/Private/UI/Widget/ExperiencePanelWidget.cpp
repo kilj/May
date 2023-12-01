92bbee33f0edc014d6215ffa6245ce6822924a50
Alexander Shapoval <kilj@me.com>
2023-12-01 16:51:33 +0200

    Added WBP_ExperiencePanel.

---
+++ Source/May/Private/UI/Widget/ExperiencePanelWidget.cpp
@@ -0,0 +1,9 @@
+﻿// Red Beat, 2023
+
+#include "UI/Widget/ExperiencePanelWidget.h"
+
+void UExperiencePanelWidget::InitWidget(const FExperiencePanelWidgetData Data) {
+	WidgetData = Data;
+
+	OnWidgetInited();
+}
---
+++ Source/May/Public/UI/Widget/ExperiencePanelWidget.h
@@ -0,0 +1,40 @@
+﻿// Red Beat, 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MayWidget.h"
+#include "ExperiencePanelWidget.generated.h"
+
+USTRUCT(BlueprintType)
+struct FExperiencePanelWidgetData {
+	GENERATED_BODY()
+
+	FExperiencePanelWidgetData() {}
+	FExperiencePanelWidgetData(UMayAbilitySystemComponent* ASC, UMayAttributeSet* AS) : AbilitySystemComponent(ASC), AttributeSet(AS) {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UMayAbilitySystemComponent> AbilitySystemComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UMayAttributeSet> AttributeSet = nullptr;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class MAY_API UExperiencePanelWidget : public UMayWidget {
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="May|Widget")
+	void InitWidget(const FExperiencePanelWidgetData Data);
+
+	UFUNCTION(BlueprintCallable, Category="May|Widget")
+	FORCEINLINE FExperiencePanelWidgetData GetWidgetData() const { return WidgetData; };
+
+private:
+	UPROPERTY()
+	FExperiencePanelWidgetData WidgetData;
+};
