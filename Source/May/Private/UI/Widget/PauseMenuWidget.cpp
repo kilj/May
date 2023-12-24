eaad931bc8a198cc42d201407b06074cf48b9119
Alexander Shapoval <kilj@me.com>
2023-12-25 00:03:35 +0200

    Added WBP_PauseMenu.

---
+++ Source/May/Private/UI/Widget/PauseMenuWidget.cpp
@@ -0,0 +1,10 @@
+﻿// Red Beat, 2023
+
+
+#include "UI/Widget/PauseMenuWidget.h"
+
+void UPauseMenuWidget::InitWidget(const FPauseMenuWidgetData Data) {
+	WidgetData = Data;
+	
+	OnWidgetInited();
+}
---
+++ Source/May/Public/UI/Widget/PauseMenuWidget.h
@@ -0,0 +1,33 @@
+﻿// Red Beat, 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MayWidget.h"
+#include "PauseMenuWidget.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPauseMenuWidgetData {
+	GENERATED_BODY()
+
+	FPauseMenuWidgetData() {}
+};
+
+/**
+ * 
+ */
+UCLASS()
+class MAY_API UPauseMenuWidget : public UMayWidget {
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="May|Widget")
+	void InitWidget(const FPauseMenuWidgetData Data);
+
+	UFUNCTION(BlueprintCallable, Category="May|Widget")
+	FORCEINLINE FPauseMenuWidgetData GetWidgetData() const { return WidgetData; };
+
+private:
+	UPROPERTY()
+	FPauseMenuWidgetData WidgetData;
+};
